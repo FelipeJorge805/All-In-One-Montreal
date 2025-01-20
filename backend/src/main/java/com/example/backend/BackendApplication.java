9ebeb62fefe6aabe067944fcf506726915c828ce
FelipeJorge805 <feliperkj805@gmail.com>
2025-01-19 19:31:05 -0500

    Creating an Event object using Spring boot Beans, using setters to add data and service to send data to the EventRepo.

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,31 @@
 package com.example.backend;
 
+import com.example.backend.model.Event;
+import com.example.backend.service.EventService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.sql.Date;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		ApplicationContext context =  SpringApplication.run(BackendApplication.class, args);
+
+		Event e = context.getBean(Event.class);
+		e.setTitle("Ice skating");
+		e.setDescription("Have fun with family");
+		e.setLocation("Place des arts");
+		e.setDate(Date.valueOf("2024-12-24"));
+		e.setUrl("www.google.com");
+
+		System.out.println(e);
+
+		EventService service = context.getBean(EventService.class);
+
+		service.addEvent(e);
 	}
 
 }
