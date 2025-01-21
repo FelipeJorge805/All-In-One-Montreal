2624962f0268ad2d776f16343bc2936150321fd8
FelipeJorge805 <feliperkj805@gmail.com>
2025-01-21 13:12:29 -0500

    Added constructor injection, fixed column name in findAll method

--- backend/src/main/java/com/example/backend/repo/EventRepo.java
+++ backend/src/main/java/com/example/backend/repo/EventRepo.java
@@ -1,6 +1,7 @@
 package com.example.backend.repo;
 
 import com.example.backend.model.Event;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -10,12 +11,10 @@ import java.util.List;
 @Repository
 public class EventRepo {
 
+    @Autowired
     private JdbcTemplate jdbc;
-    private List<Event> events;
 
-    public EventRepo() {
-        events = findAll();
-    }
+    private List<Event> events;
 
     public JdbcTemplate getJdbc() {
         return jdbc;
@@ -23,6 +22,12 @@ public class EventRepo {
     @Autowired
     public void setJdbc(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
+    }
+
+    @Autowired
+    public EventRepo(JdbcTemplate jdbc) {
+        setJdbc(jdbc);
+        events = findAll();
     }
 
     public void save(Event e) {
@@ -35,12 +40,12 @@ public class EventRepo {
 
     public List<Event> findAll() {
 
-        String sql = "select * from events";
+        String sql = "select * from event";
 
         return jdbc.query(sql, (rs,row) ->
                 new Event(
                     rs.getInt("sid"),
-                    rs.getString("name"),
+                    rs.getString("title"),
                     rs.getString("description"),
                     rs.getString("location"),
                     rs.getDate("dt"),
