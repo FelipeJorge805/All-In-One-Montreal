c6ba68dff70851af522ef7e7ee07659196a8262d
FelipeJorge805 <feliperkj805@gmail.com>
2025-01-25 16:01:14 -0500

    Added EventRestControllerTest file with no tests yet

---
+++ backend/src/test/java/com/example/backend/EventRestControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EventRestControllerTest {
+
+    @Test
+    void getAllEventsTest() {
+
+    }
+}
