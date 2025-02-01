6ab6fb27324d43a1ec19ccdd08d1d8473db615ef
FelipeJorge805 <feliperkj805@gmail.com>
2025-01-31 19:16:16 -0500

    Added findByTitle in repoJPA

--- backend/src/main/java/com/example/backend/repo/EventRepoJPA.java
+++ backend/src/main/java/com/example/backend/repo/EventRepoJPA.java
@@ -3,7 +3,9 @@ package com.example.backend.repo;
 import com.example.backend.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepoJPA extends JpaRepository<Event, Integer> {
 
-
+    List<Event> findByTitle(String title);
 }
