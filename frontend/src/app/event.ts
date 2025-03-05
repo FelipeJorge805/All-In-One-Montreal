b160083083dbea15743920ca38428fd6c7ec4bff
FelipeJorge805 <feliperkj805@gmail.com>
2025-03-05 14:27:34 -0500

    Added attributes to event class and full constructor

--- frontend/src/app/event.ts
+++ frontend/src/app/event.ts
@@ -1,2 +1,19 @@
-export class Event {
+export default class Event {
+sid: number;
+title: String;
+description: String;
+location: String;
+date: Date;
+image: String;
+url: String;
+
+    constructor(sid: number, title: String, description: String, location: String, date: Date, image: String, url: String) {
+        this.sid = sid;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.date = date;
+        this.image = image;
+        this.url = url;
+    }
 }
